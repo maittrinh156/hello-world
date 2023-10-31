26d2aa506c61119a9952c963fdab41245de0ba20
Trinh Huynh <trinhhuynh@Trinhs-MacBook-Pro.local>
2023-10-31 13:40:00 +0700

    Done task 2.

--- src/main/kotlin/com/acme/hellospring/controllers/HelloSpringController.kt
+++ src/main/kotlin/com/acme/hellospring/controllers/HelloSpringController.kt
@@ -2,12 +2,14 @@ package com.acme.hellospring.controllers
 
 import com.acme.hellospring.models.Greeting
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class GreetingController() {
-    @GetMapping("/v1")
-    fun greeting(): Greeting {
-        return Greeting("hello world")
+    @GetMapping("/v1/hello/world")
+    fun greeting(@RequestParam("name") name: String?): Greeting {
+        return Greeting("hello ${(name ?: "world")}")
     }
+
 }
