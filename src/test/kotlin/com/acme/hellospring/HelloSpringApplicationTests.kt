560ca202f67072cd07bd287ab503cff120d63cdf
Trinh Huynh <trinhhuynh@Trinhs-MacBook-Pro.local>
2023-10-31 14:44:22 +0700

    Done task 3.

--- src/test/kotlin/com/acme/hellospring/HelloSpringApplicationTests.kt
+++ src/test/kotlin/com/acme/hellospring/HelloSpringApplicationTests.kt
@@ -1,13 +1,20 @@
 package com.acme.hellospring
 
+import com.acme.hellospring.controllers.GreetingController
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class HelloSpringApplicationTests {
 
+	@Autowired
+	private var greetingController: GreetingController? = null
+
 	@Test
 	fun contextLoads() {
+		assertThat(greetingController).isNotNull()
 	}
 
 }
---
+++ src/test/kotlin/com/acme/hellospring/controllers/GreetingControllerTest.kt
@@ -0,0 +1,32 @@
+package com.acme.hellospring.controllers
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.hamcrest.Matchers.containsString
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class GreetingControllerTest {
+        @Autowired
+        private var mockMvc: MockMvc? = null;
+
+
+        @Test
+        fun greetingShouldReturnDefaultMessage() {
+                this.mockMvc?.perform(get("/v1/hello/world"))?.andExpect(status().isOk())
+                        ?.andExpect(content().string(containsString("hello world")));
+        }
+
+        @Test
+        fun greetingShouldReturnSpecificMessage() {
+                this.mockMvc?.perform(get("/v1/hello/world").param("name", "Trinh"))?.andExpect(status().isOk())
+                        ?.andExpect(content().string(containsString("hello Trinh")));
+        }
+}
